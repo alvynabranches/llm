2cde473b2236acaf8b51470f0d95c5cca47abf14
Alvyn Abranches <alvynabranches@gmail.com>
2023-07-29 19:34:49 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 sudo apt update && sudo apt install python3-pip docker.io -y
-pip install transformers torch einops accelerate xformers fastapi uvicorn --break-system-packages --no-warn-script-location
+pip install transformers torch einops accelerate xformers fastapi uvicorn --no-warn-script-location
 python3 -c "from transformers.utils import default_cache_path as dcp; print(dcp)"
