b7be1931c1d6e7af1b091c2b39adfb832f72f094
Alvyn Abranches <alvynabranches@gmail.com>
2023-08-08 15:41:00 +0530

    Create install.sh

---
+++ testing/install.sh
@@ -0,0 +1,7 @@
+df --block-size=GB
+apt install bpytop -y
+pip3 install torch --index-url https://download.pytorch.org/whl/cu118
+pip3 install transformers accelerate
+pip3 install slack_sdk
+df --block-size=GB
+huggingface-cli login
